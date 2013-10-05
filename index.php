5634889dcf0815432f814ec6ad272d681d69fcfd
LehPB <leticia.pb.00@gmail.com>
2013-10-05 13:48:04 -0300

    Subindo a estrutura

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<html>
+    <head>
+        <title>Exact-Web</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap-responsive.css"  rel="stylesheet">
+        <link href="css/Exact.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="cabecalho">ENTRA O LOGO</div>
+        <div class="row-fluid">
+            <div class="span8" id="empresa">empresa</div>
+            <div class="span4">serviço</div>
+            <div class="span4">contato</div>
+        </div>
+        <div id="rodape">RODAPE</div>
+    </body>
+</html>
