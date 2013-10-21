9660fb25a2bf311b32f94db59fb8e930898fedf1
wssimoes <walter@exactweb.com.br>
2013-10-21 01:31:58 -0200

    ajustes

--- index.php
+++ index.php
@@ -3,19 +3,48 @@
         <title>Exact-Web</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <link href="css/Exact.css" type="text/css" rel="stylesheet">
+        <link href="jsmetro/MetroJs.js" type="text/javascript">
+        <link href="jsmetro/MetroJs.min.js" type="text/javascript">
+        <link href="css/bootstrap/css/bootstrap.css" type="text/css" rel="stylesheet">
+        <link href="css/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+        <link href="css/bootstrap/bootstrap-responsive.css" rel="stylesheet">
+        <link href="css/exact.css" rel="stylesheet">
     </head>
     <body>
-        
-        <div id="cabecalho">
-            <div id="logo"><img src="imagem/logo.jpg"></div>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner header"><img src="imagem/logo.png"></div>
+            
+            <div class="conteudo">
+                <ul class="thumbnails">
+                    <li class="span4">
+                        <div class="thumbnail">
+                            <p>IMAGEM</p>  
+                            <h3>Empresa</h3> 
+                        </div>
+                    </li>
+                    
+                    <li class="span4">
+                        <div class="thumbnail">
+                            <p>IMAGEM</p>  
+                            <h3>Serviços</h3>
+                        </div>
+                    </li>
+                    
+                    <li class="span4">
+                        <div class="thumbnail">
+                            <p>IMAGEM</p>  
+                            <h3>Contato</h3>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="navbar navbar-inverse navbar-fixed-bottom">
+                <div class="navbar-inner text-center">
+                    </br>
+                    <p style="color: #ffffff;">&copy; <?php echo date("Y") ?> - Exactweb - soluções em internet</p>
+                </div> 
+            </div>
         </div>
-        
-        <div id="conteudo">
-            x
-        </div>
-        
-        <div id="rodape">&copy; <?php echo date("Y")?> Exact-Web Soluções para internet</div>
     </body>
 </html>
